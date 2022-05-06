8426cea037ee3feb8b8762818bfe9b5e2e7324cc
RX-Rexax <80836157+RX-Rexax@users.noreply.github.com>
2022-05-06 11:31:41 +0800

    Initial Commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    bool gameHasEnded = false;
+    public GameObject gameOverUI;
+
+    public void EndGame()
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("Game Over!");
+            SceneManager.LoadScene("Main Menu");
+        }
+    }
+
+    public void GameOver()
+    {
+        gameOverUI.SetActive(true);
+        Invoke("EndGame", 3f);
+    }
+
+}
---
+++ Assets/Scripts/pauseMenu.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pauseMenu : MonoBehaviour
+{
+
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown("p"))
+        {
+            if (GameIsPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+
+        }
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenuUI.SetActive(false);       // Hide the PauseMenu
+        Time.timeScale = 1f;                // Set the game to normal speed (Resume Game)
+        GameIsPaused = false;
+    }
+
+    void PauseGame()
+    {
+        pauseMenuUI.SetActive(true);        // Show the PauseMenu 
+        Time.timeScale = 0f;                // Freeze the game (Pause the game)
+        GameIsPaused = true;
+    }
+
+    public void quitGame()
+    {
+        Time.timeScale = 1f;                // Continue the game (if not the game is frozen)
+        FindObjectOfType<GameManager>().EndGame();
+        
+    }
+}
